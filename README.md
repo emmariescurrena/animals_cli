76088312af1f48427550091e1334bac015bc1ea2
emmariescurrena <emmariescurrena>
2024-04-08 08:22:42 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Animals CLI Rust Project 
+
+This projects consists in commands that creates and make interactions between animals.
+
+## Commands available:
+
+
+## Applied knowledge (personal thing):
---
+++ target/debug/deps/animals_cli-401797c3533c378f.d
@@ -0,0 +1,5 @@
+/home/emmanuel/no-borrar/repos/mi-vault/programming/blockchain_development/rust/projects/animals_cli/target/debug/deps/libanimals_cli-401797c3533c378f.rmeta: src/main.rs
+
+/home/emmanuel/no-borrar/repos/mi-vault/programming/blockchain_development/rust/projects/animals_cli/target/debug/deps/animals_cli-401797c3533c378f.d: src/main.rs
+
+src/main.rs:
